e3ecf09971f9c30ced1651ab794f326e8a186381
Thanh Nguyen <puffle.nnt@gmail.com>
2021-10-17 01:06:21 +0700

    menu configured

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const ipc = require('electron').ipcMain
 const dialog = require('electron').dialog
+const meny = require('electron').Menu
+
+const isMac = process.platform === 'darwin'
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -9,33 +12,124 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            enableRemoteModule: true
         }
     })
     win.loadFile('src/index.html');
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
     createWindow();
 
-    app.on('activate', function() {
-        if(BrowserWindow.getAllWindows().length === 0) {
+    const template = [
+        //File menu
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: "Open File...",
+                    click: function () {
+                        dialog.showOpenDialog({
+                            properties: ['openFile', "multiSelections"]
+                        })
+                    }
+                },
+                {
+                    label: "Open Folder...",
+                    click: () => {
+                        dialog.showOpenDialog({
+                            properties: ['openDirectory', "multiSelections"]
+                        })
+                    }
+                },
+                isMac ? { role: 'close' } : { role: 'quit' },
+            ]
+        },
+        
+        //Edit menu
+        {
+            label: 'Edit',
+            submenu: [
+              { role: 'undo' },
+              { role: 'redo' },
+              { type: 'separator' },
+              { role: 'cut' },
+              { role: 'copy' },
+              { role: 'paste' },
+              ...(isMac ? [
+                { role: 'pasteAndMatchStyle' },
+                { role: 'delete' },
+                { role: 'selectAll' },
+                { type: 'separator' },
+                {
+                  label: 'Speech',
+                  submenu: [
+                    { role: 'startSpeaking' },
+                    { role: 'stopSpeaking' }
+                  ]
+                }
+              ] : [
+                { role: 'delete' },
+                { type: 'separator' },
+                { role: 'selectAll' }
+              ])
+            ]
+          },
+
+          //View menu
+          {
+            label: 'View',
+            submenu: [
+              { role: 'reload' },
+              { role: 'forceReload' },
+              { role: 'toggleDevTools' },
+              { type: 'separator' },
+              { role: 'resetZoom' },
+              { role: 'zoomIn' },
+              { role: 'zoomOut' },
+              { type: 'separator' },
+              { role: 'togglefullscreen' }
+            ]
+          },
+
+          //Window menu
+          {
+            label: 'Window',
+            submenu: [
+              { role: 'minimize' },
+              { role: 'zoom' },
+              ...(isMac ? [
+                { type: 'separator' },
+                { role: 'front' },
+                { type: 'separator' },
+                { role: 'window' }
+              ] : [
+                { role: 'close' }
+              ])
+            ]
+          },
+    ]
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
         }
     })
 })
 
-app.on('window-all-closed', function() {
-    if(process.platform !== 'darwin') {
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 })
 
 ipc.on('open-file-dialog', function (event) {
     dialog.showOpenDialog({
-      properties: ['openFile', 'openDirectory']
+        properties: ['openFile', "multiSelections"]
     }, function (files) {
-      if (files) event.sender.send('selected-file', files)
+        if (files) event.sender.send('selected-file', files)
     })
-  })
+})
