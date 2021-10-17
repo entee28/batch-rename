78551ad64039311630eacb3a575939d1cdb376c1
Thanh Nguyen <puffle.nnt@gmail.com>
2021-10-17 16:09:33 +0700

    open dialog handled

--- src/main.js
+++ src/main.js
@@ -6,130 +6,149 @@ const meny = require('electron').Menu
 const isMac = process.platform === 'darwin'
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        }
-    })
-    win.loadFile('src/index.html');
-    // win.webContents.openDevTools();
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  })
+  win.loadFile('src/index.html');
+  // win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    const template = [
-        //File menu
+  const template = [
+    //File menu
+    {
+      label: 'File',
+      submenu: [
         {
-            label: 'File',
-            submenu: [
-                {
-                    label: "Open File...",
-                    click: function () {
-                        dialog.showOpenDialog({
-                            properties: ['openFile', "multiSelections"]
-                        })
-                    }
-                },
-                {
-                    label: "Open Folder...",
-                    click: () => {
-                        dialog.showOpenDialog({
-                            properties: ['openDirectory', "multiSelections"]
-                        })
-                    }
-                },
-                isMac ? { role: 'close' } : { role: 'quit' },
-            ]
+          label: "Open File...",
+          accelerator: "Ctrl+O",
+          click() {
+            openFile();
+          }
         },
-        
-        //Edit menu
         {
-            label: 'Edit',
-            submenu: [
-              { role: 'undo' },
-              { role: 'redo' },
-              { type: 'separator' },
-              { role: 'cut' },
-              { role: 'copy' },
-              { role: 'paste' },
-              ...(isMac ? [
-                { role: 'pasteAndMatchStyle' },
-                { role: 'delete' },
-                { role: 'selectAll' },
-                { type: 'separator' },
-                {
-                  label: 'Speech',
-                  submenu: [
-                    { role: 'startSpeaking' },
-                    { role: 'stopSpeaking' }
-                  ]
-                }
-              ] : [
-                { role: 'delete' },
-                { type: 'separator' },
-                { role: 'selectAll' }
-              ])
-            ]
-          },
+          label: "Open Folder...",
+          click() {
+            openFolder();
+          }
+        },
+        isMac ? { role: 'close' } : { role: 'quit' },
+      ]
+    },
 
-          //View menu
+    //Edit menu
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac ? [
+          { role: 'pasteAndMatchStyle' },
+          { role: 'delete' },
+          { role: 'selectAll' },
+          { type: 'separator' },
           {
-            label: 'View',
+            label: 'Speech',
             submenu: [
-              { role: 'reload' },
-              { role: 'forceReload' },
-              { role: 'toggleDevTools' },
-              { type: 'separator' },
-              { role: 'resetZoom' },
-              { role: 'zoomIn' },
-              { role: 'zoomOut' },
-              { type: 'separator' },
-              { role: 'togglefullscreen' }
+              { role: 'startSpeaking' },
+              { role: 'stopSpeaking' }
             ]
-          },
+          }
+        ] : [
+          { role: 'delete' },
+          { type: 'separator' },
+          { role: 'selectAll' }
+        ])
+      ]
+    },
 
-          //Window menu
-          {
-            label: 'Window',
-            submenu: [
-              { role: 'minimize' },
-              { role: 'zoom' },
-              ...(isMac ? [
-                { type: 'separator' },
-                { role: 'front' },
-                { type: 'separator' },
-                { role: 'window' }
-              ] : [
-                { role: 'close' }
-              ])
-            ]
-          },
-    ]
+    //View menu
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
 
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+    //Window menu
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac ? [
+          { type: 'separator' },
+          { role: 'front' },
+          { type: 'separator' },
+          { role: 'window' }
+        ] : [
+          { role: 'close' }
+        ])
+      ]
+    },
+  ]
 
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    })
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
 })
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 })
 
 ipc.on('open-file-dialog', function (event) {
-    dialog.showOpenDialog({
-        properties: ['openFile', "multiSelections"]
-    }, function (files) {
-        if (files) event.sender.send('selected-file', files)
-    })
+  const files = openFile();
+  if (files) event.sender.send('selected-file', files);
 })
+
+ipc.on('open-folder-dialog', function (event) {
+  const folders = openFolder();
+  if (folders) event.sender.send('selected-folder', folders);
+})
+
+const openFile = () => {
+  const files = dialog.showOpenDialogSync({ properties: ['openFile', 'multiSelections'] });
+
+  if (!files) { return; }
+  return files;
+}
+
+const openFolder = () => {
+  const folders = dialog.showOpenDialogSync({ properties: ['openDirectory', 'multiSelections'] });
+
+  if (!folders) { return; }
+  return folders;
+}
+
+  // dialog.showOpenDialog({
+  //     properties: ['openFile', "multiSelections"]
+  // }, function (files) {
+  //     if (files) event.sender.send('selected-file', files)
+  // })
--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,19 @@
 const ipc = require('electron').ipcRenderer
 
-const openBtn = document.getElementById('openFileBtn');
-openBtn.addEventListener('click', function(event) {
+const openFileBtn = document.getElementById('openFileBtn');
+openFileBtn.addEventListener('click', function(event) {
     ipc.send('open-file-dialog')
+})
+
+const openFolderBtn = document.getElementById('openFolderBtn');
+openFolderBtn.addEventListener('click', function(event) {
+    ipc.send('open-folder-dialog')
+})
+
+ipc.on('selected-file', function(event, files) {
+    //received files from main process, need handle
+})
+
+ipc.on('selected-folder', function(event, folders) {
+    //received files from main process, need handle
 })
