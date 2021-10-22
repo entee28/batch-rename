9e103ba6500986eb5e4950b1bb472c2ff869cebd
Thanh Nguyen <puffle.nnt@gmail.com>
2021-10-22 09:23:51 +0700

    added drag and drop handle

--- src/renderer.js
+++ src/renderer.js
@@ -17,3 +17,18 @@ ipc.on('selected-file', function(event, files) {
 ipc.on('selected-folder', function(event, folders) {
     //received files from main process, need handle
 })
+
+document.addEventListener('drop', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  
+    for (const f of event.dataTransfer.files) {
+        // Using the path attribute to get absolute file path
+        console.log('File Path of dragged files: ', f.path)
+      }
+});
+  
+document.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
