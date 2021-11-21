0b5fce285531f8531fc5131bf417bea07de41783
Thanh Nguyen <puffle.nnt@gmail.com>
2021-11-21 15:37:30 +0700

    fixed glitch

--- src/renderer.js
+++ src/renderer.js
@@ -289,9 +289,9 @@ const addFileItem = (__filepath) => {
 
 //Function handle adding delete button for each loaded file
 function addDelButton(parent) {
-    const delBtn = parent.appendChild(document.createElement("button"));
-    delBtn.classList.add("btn");
-    const delIcon = document.createElement("td");
+    const delBtn = parent.appendChild(document.createElement("td"));
+    // delBtn.classList.add("btn");
+    const delIcon = document.createElement("button");
     delIcon.classList.add("fa");
     delIcon.classList.add("fa-trash");
     delBtn.appendChild(delIcon);
